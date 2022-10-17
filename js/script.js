0417ea07ed0df4a55f6e8a41a3eb07d7accaecd4
UmbreonX2 <matheuslivio2328@gmail.com>
2022-10-16 22:18:43 -0300

    select

--- js/script.js
+++ js/script.js
@@ -11,13 +11,16 @@ class MobileNavbar {
   animateLinks() {
     this.navLinks.forEach((link, index) => {
       link.style.animation
-      ? (link.style.animation = "")
-      : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s ${index / 7 + 0.3}`);
-    })
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+        }s`);
+    });
   }
 
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
     this.animateLinks();
   }
 
